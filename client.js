632337a09c1380981f05f783f65a0ad67ec5b434
hehechuppy <nguyenlehoangsang111@gmail.com>
2026-08-12 11:02:11 +0700

    Add files via upload

---
+++ client.js
@@ -0,0 +1,13 @@
+// client.js
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+module.exports = client;
