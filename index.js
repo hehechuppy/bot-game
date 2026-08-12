27a5c7c7bc7f5a248e9d46557ae9378324219b85
hehechuppy <nguyenlehoangsang111@gmail.com>
2026-08-12 12:56:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,4 @@ http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Bot is running');
 }).listen(PORT, () => {
-    console.log(✅ Server listening on port ${PORT});
-});
-Đó
+    console
